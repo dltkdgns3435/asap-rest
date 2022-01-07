40eb45a52777735abcedcec00de18663890aac87
sh-mac <dltkdgns3435@naver.com>
2022-01-07 09:16:20 +0900

    Initial commit

---
+++ src/main/java/com/worryswat/asaprest/AsapRestApplication.java
@@ -0,0 +1,13 @@
+package com.worryswat.asaprest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsapRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsapRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/worryswat/asaprest/AsapRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.worryswat.asaprest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsapRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
