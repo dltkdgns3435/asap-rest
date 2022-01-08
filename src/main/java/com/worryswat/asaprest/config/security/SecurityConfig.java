18893ab98a0fa5dc1c76cbb989ae19e4abe77ba1
sh-mac <dltkdgns3435@naver.com>
2022-01-09 04:35:36 +0900

    #3: 시큐리티 설정

--- src/main/java/com/worryswat/asaprest/config/security/SecurityConfig.java
+++ src/main/java/com/worryswat/asaprest/config/security/SecurityConfig.java
@@ -35,6 +35,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .headers()
                 .frameOptions()
                 .sameOrigin();
+
         http
                 .authorizeRequests()
                 .antMatchers("/**").permitAll()
