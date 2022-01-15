baeb444f9aef1b25a5d6f18b8f4113cfde3842c5
sh-mac <dltkdgns3435@naver.com>
2022-01-15 15:14:48 +0900

    #8: user 테이블 설계

--- src/main/java/com/worryswat/asaprest/entity/User.java
+++ src/main/java/com/worryswat/asaprest/entity/User.java
@@ -2,10 +2,12 @@ package com.worryswat.asaprest.entity;
 
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Getter
 @NoArgsConstructor
@@ -19,4 +21,26 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_idx_seq")
     private Long id;
+
+    @Column(length = 20, nullable = false)
+    private String nickname;
+
+    @Column(length = 15, nullable = false)
+    private String ip;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_id")
+    private List<Question> questions;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_id")
+    private List<Answer> answers;
+
+    @Builder
+    public User(String nickname, String ip, List<Question> questions, List<Answer> answers) {
+        this.nickname = nickname;
+        this.ip = ip;
+        this.questions = questions;
+        this.answers = answers;
+    }
 }
